667edca99a54a933d7fd8820211235c1c4b8c55b
abhaybhatt <abhaybhatt1732000@gmail.com>
2022-02-15 23:11:25 +0530

    removed unused code

--- src/pages/AuthenticationPages/Login.js
+++ src/pages/AuthenticationPages/Login.js
@@ -53,15 +53,6 @@ const Login = () => {
                 </div>
             </div>
         </div>
-
-        {/* <footer>
-            <p>
-                Created with <i className="fa fa-heart"></i> by
-                <a target="_blank" href="https://florin-pop.com">Florin Pop</a>
-                - Read how I created this and how you can join the challenge
-                <a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
-            </p>
-        </footer> */}
         </>
     )
 }
